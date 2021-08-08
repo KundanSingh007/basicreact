31498a121833656aded55bef25d72109ecab1517
Kundan Singh <35429799+KundanSingh007@users.noreply.github.com>
2021-08-08 19:28:03 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [data, setData] = useState("kundan");
+  const [hello, setHello] = useState("run");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{data}</h1>
+      <button onClick={() => setData("singh")}>Update data</button>
+
+      <h2>{hello}</h2>
+      <button onClick={()=>setHello("byeeee")}>hello change</button>
     </div>
   );
 }
