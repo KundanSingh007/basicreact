29ab432093029cce3bc747a125517a1deda57b31
Kundan Singh <35429799+KundanSingh007@users.noreply.github.com>
2021-08-19 00:58:31 +0530

    updates

--- src/Components/atm/atm.jsx
+++ src/Components/atm/atm.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "./atm.css";
-import atms from "./visacard.png";
+import visas from "./visacard.png";
 
 function Atm() {
   return (
-    <div>
-      <img src={atms} alt="" />
+    <div id="atm">
+      <h6 id="cente">Sign up in minutes</h6>
+      <h1>Fast accounting opening</h1>
+      <p>
+        Online education is approachable in many ways,but students often neglect
+        the fact that it is much more economical.
+      </p>
+      <button id="get__started">Get Started</button>
+      <img src={visas} alt="" />
     </div>
   );
 }
-
 export default Atm;
